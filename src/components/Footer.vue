be43ec3f202a923b71dbc5a254fd6ba8bca3795d
lveMonsi <109103173+lveMonsi@users.noreply.github.com>
2025-03-02 21:28:51 +0800

    Update Footer.vue

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -23,7 +23,7 @@
         <!-- 站点备案 -->
         <span>
           &amp;
-          <a v-if="siteIcp" href="https://beian.miit.gov.cn" target="_blank">
+          <a v-if="siteIcp" href="https://icp.gov.moe/?keyword=20251086" target="_blank">
             {{ siteIcp }}
           </a>
         </span>
